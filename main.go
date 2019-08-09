980974b4e4a8724e16f5a8eabad7787d85d63a36
Maxim Yefremov <maxYefr@gmail.com>
2019-08-09 11:38:36 +0300

    listening from 0,1,2 partitions

--- main.go
+++ main.go
@@ -9,27 +9,34 @@ import (
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Llongfile)
-	conf := kafka.ReaderConfig{
-		Brokers:   []string{"localhost:9092"},
-		Topic:     "second_topic",
-		Partition: 0,
-		MinBytes:  10e3, // 10KB
-		MaxBytes:  10e6, // 10MB
+	partions := []int{0, 1, 2}
+	for _, p := range partions {
+		go func(p int) {
+			conf := kafka.ReaderConfig{
+				Brokers:   []string{"localhost:9092"},
+				Topic:     "second_topic",
+				Partition: p,
+				MinBytes:  10e3, // 10KB
+				MaxBytes:  10e6, // 10MB
+			}
+			// make a new reader that consumes from topic-A, partition 0, at offset 42
+			r := kafka.NewReader(conf)
+			//r.SetOffset(42)
+
+			log.Println("conf", conf)
+
+			for {
+				m, err := r.ReadMessage(context.Background())
+				if err != nil {
+					log.Println(err)
+					break
+				}
+				log.Printf("partition: %+v, message at offset %d: %s = %s\n", p, m.Offset, string(m.Key), string(m.Value))
+			}
+
+			r.Close()
+		}(p)
 	}
-	// make a new reader that consumes from topic-A, partition 0, at offset 42
-	r := kafka.NewReader(conf)
-	//r.SetOffset(42)
-
-	log.Println("conf", conf)
-
-	for {
-		m, err := r.ReadMessage(context.Background())
-		if err != nil {
-			log.Println(err)
-			break
-		}
-		log.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
-	}
-
-	r.Close()
+	forever := make(chan bool)
+	<-forever
 }
